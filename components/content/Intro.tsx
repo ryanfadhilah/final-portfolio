2d5c1cd15e42a1cdc1edfe4fa8aae5512cab803c
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-09-05 21:30:15 +0700

    90%

--- components/content/Intro.tsx
+++ components/content/Intro.tsx
@@ -24,8 +24,8 @@ export default function Intro({}: Props) {
     >
       {/* Profile */}
       <div
-        className="flex flex-col gap-5
-        mx-5
+        className="flex flex-col gap-5 
+        mx-5 
       md:mx-0 "
       >
         <motion.h1
@@ -66,13 +66,14 @@ export default function Intro({}: Props) {
         </div>
       </div>
 
+      {/* Photo Large */}
       <motion.div
         initial={{ scale: 0.5 }}
         animate={{ scale: 1 }}
         transition={{ duration: 1.5, ease: [0.6, 0.01, -0.05, 0.9] }}
-        className="flex flex-col items-center justify-center bg-black 
-        px-5 pt-10 gap-5
-      md:gap-10 md:px-10 md:py-20"
+        className="hidden bg-black 
+        px-5 pt-10 gap-5 mt-40
+      md:gap-10 md:px-10 md:py-20 md:mt-0 md:flex md:flex-col md:items-center md:justify-center"
       >
         <Image
           src={"/profiles.jpeg"}
@@ -81,6 +82,24 @@ export default function Intro({}: Props) {
           height={400}
         ></Image>
         <h1 className="text-white text-xl flex w-full">Ryan Fadhilah</h1>
+      </motion.div>
+
+      {/* Photo Small */}
+      <motion.div
+        initial={{ scale: 0.5 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1.5, ease: [0.6, 0.01, -0.05, 0.9] }}
+        className="flex  justify-center items-center gap-5
+        md:hidden"
+      >
+        <h1 className="text-2xl font-bold">Ryan Fadhilah</h1>
+        <Image
+          src={"/profiles.jpeg"}
+          alt="photo"
+          width={150}
+          height={150}
+          className="rounded-full"
+        ></Image>
       </motion.div>
     </motion.main>
   );
